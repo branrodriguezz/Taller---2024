303cb47c62483e9fb9a63ea0bf713ac0d1d49b10
branrodriguezz <bran3rodriguez@gmail.com>
2024-09-01 11:20:07 -0300

    Practica 3 - Ej2.pas

--- Practica 3 - Ej2.pas
+++ Practica 3 - Ej2.pas
@@ -9,16 +9,25 @@ iii. Generar y retornar otro árbol binario de búsqueda de productos vendidos o
  la lista de las ventas realizadas del producto.
 Nota: El módulo debe retornar TRES árboles.
 b. Implemente un módulo que reciba el árbol generado en i. y una fecha y retorne la cantidad total de productos vendidos en la fecha recibida.
-c. Implemente un módulo que reciba el árbol generado en ii. y retorne el código de producto
-con mayor cantidad total de unidades vendidas.
-c. Implemente un módulo que reciba el árbol generado en iii. y retorne el código de producto con mayor cantidad de ventas.}
+c. Implemente un módulo que reciba el árbol generado en ii. y retorne el código de producto con mayor cantidad total de unidades vendidas.
+d. Implemente un módulo que reciba el árbol generado en iii. y retorne el código de producto con mayor cantidad de ventas.}
 
-program ej1p3;
-Type
+program prueba;
+const
+	max = 5;
+type
+
+	// -------------------- arbol1 ---------------
+	fecha = record
+		dia: integer;
+		mes: integer;
+		anio: integer;
+	end;
+	
 	venta = record
-		cod: integer;
-		fecha: integer;
-		cant: integer;
+		codigo: integer;
+		fecha: fecha;
+		cantidad: integer;
 	end;
 	
 	arbol1 = ^nodo;
@@ -27,9 +36,11 @@ Type
 		hi: arbol1;
 		hd: arbol1;
 	end;
+	// -------------------- arbol1 ---------------
 	
+	// -------------------- arbol 2 ---------------
 	producto = record
-		cod: integer;
+		codigo: integer;
 		total: integer;
 	end;
 	
@@ -39,6 +50,9 @@ Type
 		hi2: arbol2;
 		hd2: arbol2;
 	end;
+	//------------------- arbol2 ----------------
+	
+	// ------------------ arbol 3 ---------------
 	
 	lista = ^nodolista;
 	nodolista = record
@@ -46,101 +60,343 @@ Type
 		sig: lista;
 	end;
 	
+	produylista = record
+		codigo: integer;
+		listaa: lista;
+	end;
+	
 	arbol3 = ^nodo3;
 	nodo3 = record
-		dato3: lista; //en la lista ya esta el codigo de producto
+		dato3: produylista;
 		hi3: arbol3;
 		hd3: arbol3;
 	end;
-
+		
+	// ------------------ arbol 3 ---------------
+	
+//-----------------------genera Informacion random para cargar en los arboles ------------
 procedure generarInformacion (var v: venta);
 begin
-	v.cod:= random (100); //un producto puede estar en mas de una venta
-	v.fecha:= random (31) + 1;
-	v.cant:= random (100);
+	v.codigo:= random (100);
+	v.fecha.dia:= random (31) + 1;
+	v.fecha.mes:= random (12) + 1;
+	v.fecha.anio:= random (25) + 2000;
+	v.cantidad:= random (max);
 end;
+//-----------------------genera Informacion random para cargar en los arboles ------------
 
-procedure agregar (var a1: arbol1; v: venta);
+//-----------------------cargar arbol 1 ------------------------
+procedure cargarArbol1 (var a1: arbol1; v: venta);
 begin
 	if (a1 = nil) then begin
 		new (a1);
 		a1^.dato:= v;
-		a1^.hi:= nil; 
-		a1^.hd:= nil;
+		a1^.hi:= nil;
+		a1^.hd:= nil
 	end
 	else begin
-		if (v.cod < a1^.dato.cod) then 
-			agregar (a1^.hi, v) 
-		else 
-			agregar (a1^.hd, v);
+		if (v.codigo < a1^.dato.codigo) then
+			cargarArbol1 (a1^.hi, v)
+		else
+			cargarArbol1 (a1^.hd, v);
 	end;
 end;
+//-----------------------cargar arbol 1 ------------------------
 
-procedure cargarArbol1 (var a1: arbol1);
+//-----------------------cargar arbol 2 ------------------------
+procedure cargarArbol2 (var a2: arbol2; v: venta);
+begin
+	if (a2 = nil) then begin
+		new (a2);
+		a2^.dato2.codigo:= v.codigo;
+		a2^.dato2.total:= v.cantidad;
+		a2^.hi2:= nil;
+		a2^.hd2:= nil;
+	end
+	else begin
+		if (v.codigo = a2^.dato2.codigo) then 
+			a2^.dato2.total:= a2^.dato2.total + v.cantidad
+		else begin
+			if (v.codigo < a2^.dato2.codigo) then
+				cargarArbol2 (a2^.hi2, v)
+			else
+				cargarArbol2 (a2^.hd2, v);
+		end;
+	end;
+end;
+//-----------------------cargar arbol 2 ------------------------
+
+//-----------------------cargar arbol 3 ------------------------
+
+//----------------------- agrega adelante en la lista de ventas ------------------------
+
+procedure agregarAdelante (var l: lista; v: venta);
+var
+	nuevo: lista;
+begin
+	new (nuevo);
+	nuevo^.ele:= v;
+	nuevo^.sig:= nil;
+	if (l = nil) then
+		l:= nuevo
+	else begin
+		nuevo^.sig:= l;
+		l:= nuevo;
+	end;	
+end;
+//----------------------- agrega adelante en la lista de ventas ------------------------
+
+//-----------------------cargar arbol 3 ------------------------
+procedure cargarArbol3 (var a3: arbol3; v: venta);
+begin
+	if (a3 = nil) then begin
+		new (a3);
+		a3^.dato3.codigo:= v.codigo;
+		a3^.dato3.listaa:= nil;
+		agregarAdelante (a3^.dato3.listaa, v);
+		a3^.hi3:= nil;
+		a3^.hd3:= nil;
+	end
+	else begin
+		if (v.codigo = a3^.dato3.codigo) then begin
+			agregarAdelante (a3^.dato3.listaa, v);
+		end
+		else begin
+			if (v.codigo < a3^.dato3.codigo) then
+				cargarArbol3 (a3^.hi3, v)
+			else
+				cargarArbol3 (a3^.hd3, v);
+		end;
+	end;
+end;
+//-----------------------cargar arbol 3 ------------------------
+
+//--------------generar arboles ------------
+procedure generarArboles (var a1: arbol1; var a2: arbol2; var a3: arbol3); //tiene que ser recursivo?
 var
 	v: venta;
 begin
 	generarInformacion (v);
-	if (v.cod <> 0) then begin
-		agregar (a1, v);
-		cargarArbol1 (a1);
+	while (v. codigo <> 0) do begin
+		cargarArbol1 (a1, v);
+		cargarArbol2 (a2, v);
+		cargarArbol3 (a3, v);
+		generarInformacion (v);
 	end;
 end;
+//---------------generar arboles ------------
 
-procedure agregar (var a2: arbol2; p: producto);
+//------------imprime lista del arbol 3 ----------
+
+procedure imprimirLista (l: lista);
 begin
-	if (a2 = nil) then begin
-		new (a2);
-		a2^.dato2:= p;
-		a2^.hi2:= nil; 
-		a2^.hd2:= nil;
-	end
-	else begin
-		if (p.cod <= a2^.dato2.cod) then 
-			agregar (a2^.hi2, p)
-		else 
-			agregar (a2^.hd2, p);
+	while (l <> nil) do begin
+		write ('Venta de la fecha:');
+		write (' | ' ,l^.ele.fecha.dia,'/' ,l^.ele.fecha.mes, '/' ,l^.ele.fecha.anio, ' | ');
+		write ('| Cantidad vendida: ' , l^.ele.cantidad , ' | ');
+		l:= l^.sig;
 	end;
+	writeln;
 end;
 
-procedure cargarArbol2 (a1: arbol1; var a2: arbol2);
-var
-	act: integer;
-	p: producto;
-	cantv: integer;
+//------------imprime lista del arbol 3 ----------
+
+//------------imprime arbol 3 ----------
+
+procedure impresion3 (pl: produylista);
+begin
+	writeln;
+	write ('Producto con codigo: ' ,pl.codigo, '   ');
+	imprimirLista (pl.listaa);
+end;
+
+//------------imprime arbol 3 ----------
+
+//------------imprime arbol 3 ----------
+procedure imprimirArbol3 (a3: arbol3);
+begin
+	if (a3 <> nil) then begin
+		imprimirArbol3 (a3^.hi3);
+		impresion3 (a3^.dato3);
+		imprimirArbol3 (a3^.hd3);
+	end;
+end;
+//------------imprime arbol 3 ----------
+
+//------------imprime arbol 2 ----------
+procedure impresion2 (p: producto);
+begin
+	writeln;
+	write ('El codigo de producto : ' , p.codigo);
+	writeln;
+	writeln ('Tiene un total de productos vendidos: ' , p.total);
+	writeln;
+end;
+
+procedure imprimirArbol2 (a2: arbol2);
+begin
+	if (a2 <> nil) then begin
+		imprimirArbol2 (a2^.hi2);
+		impresion2 (a2^.dato2);
+		imprimirArbol2 (a2^.hd2);
+	end;
+end;
+//-----------imprime arbol 2 --------------
+
+//-----------imprime arbol1 --------------
+procedure impresion (v: venta);
+begin
+	write ('El codigo : ' , v.codigo);
+	writeln;
+	write ('La fecha es: ' );
+	writeln (v.fecha.dia, ' | ', v.fecha.mes, ' | ' , v.fecha.anio);
+	writeln ('La cantidad de productos vendidos es: ', v.cantidad);
+	writeln;
+end;
+
+procedure imprimirArbol1 (a1: arbol1);
 begin
 	if (a1 <> nil) then begin
-		cantv:= 0; //dudoso
-		act:= a1^.dato.cod;
-		if (act = a1^.dato.cod) then begin //aplico corte de control
-			cantv:= cantv + 1; 
-			cargarArbol2 (a1^.hi,a2);
-			cargarArbol2 (a1^.hd,a2);
+		imprimirArbol1 (a1^.hi);
+		impresion (a1^.dato);
+		imprimirArbol1 (a1^.hd);
+	end;
+end;
+//-----------imprime arbol1 --------------
+
+//------------impresion de arboles ----------
+procedure imprimirArboles (a1: arbol1; a2: arbol2; a3: arbol3);
+begin
+	writeln;
+	write ('El arbol 1 es: (en orden) ');
+	writeln;
+	imprimirArbol1 (a1);
+	writeln;
+	write ('El arbol 2 es: (en orden) ');
+	writeln; 
+	imprimirArbol2 (a2);
+	writeln;
+	write ('El arbol 3 es: (en orden) ');
+	imprimirArbol3 (a3);
+	writeln;
+end;
+//------------impresion de arboles ----------
+
+//------------- busca la fecha y suma la cant de productos -------------
+
+procedure buscardeEsaFecha (a1: arbol1; fechaa: fecha; var cantProductos: integer);
+begin
+	if (a1 <> nil) then begin
+		buscardeEsaFecha (a1^.hi, fechaa, cantProductos);
+		if (a1^.dato.fecha.dia = fechaa.dia) and (a1^.dato.fecha.mes = fechaa.mes) and (a1^.dato.fecha.anio = fechaa.anio) then
+			cantProductos:= cantProductos + a1^.dato.cantidad;
+		buscardeEsaFecha (a1^.hd, fechaa, cantProductos);
+	end;
+end;
+//------------- busca la fecha y suma la cant de productos -------------
+
+//------------- buscar cod max (sin terminar) ---------------------------
+{function buscarcodMax (a2: arbol2; maxi: integer): integer;
+begin
+	if (a2 <> nil) then begin
+		if (a2^.hd2 = nil) then begin
+			buscarcodMax:= a2^.dato2.codigo;
+		end
+		else begin
+			buscarcodMax:= buscarcodMax (a2^.hd2,maxi);
 		end;
-		p.cod:= act; p.total:= cantv;
-		agregar (a2,p);
-		cargarArbol2 (a1, a2);
+	end;
+end;}
+//------------- buscar cod max (sin terminar) ---------------------------
+
+//------------- buscar cod max con un proceso dentro de la funcionn ---------------------------
+
+procedure buscarlo (a2: arbol2; var max: integer; var maxCodigo: integer);
+begin
+	if (a2 <> nil) then begin
+		if (a2^.dato2.total > max) then begin
+			max:= a2^.dato2.total;
+			maxCodigo:= a2^.dato2.codigo;
+		end;
+		buscarlo (a2^.hi2, max, maxCodigo);
+		buscarlo (a2^.hd2, max, maxCodigo);
 	end;
 end;
 
-procedure cargarArbol3 (var a3: arbol3);
-var 
+function buscarcodMax (a2: arbol2): integer;
+var
+	max, maxCodigo: integer;
 begin
+	max:= -1;
+	maxCodigo:= -1;
+	buscarlo (a2, max, maxCodigo);
+	buscarcodMax:= maxCodigo;
 end;
 
-procedure generarArboles (var a1: arbol1; var a2: arbol2; var a3: arbol3);
+//------------- buscar cod max con un proceso dentro de la function  ---------------------------
+
+
+//------------------ busca codigo maximo del que tiene mayor cantidad de ventas del arbol 3 ---------------------
+procedure cuentoVentas (l: lista; var vent: integer);
 begin
-	cargarArbol1 (a1);
-	cargarArbol2 (a1,a2); 
-	cargarArbol3 (a3);
+	if (l <> nil) then begin
+		vent:= vent + 1;
+		cuentoVentas (l^.sig, vent);
+	end;
 end;
 
+procedure calculoMax (vent: integer; cod: integer; var codMax: integer; var maxVentas: integer);
+begin
+	if (vent > maxVentas) then begin
+		maxVentas:= vent;
+		codMax:= cod;
+	end;
+end;
+
+procedure mayorCantidadVentas (a3: arbol3; var maxCod: integer; var maxVentas: integer); //que pasa aca si dos codigos de venta tiene la misma cantidad de ventas?
+var
+	vent: integer;
+begin
+	if (a3 <> nil) then begin
+		mayorCantidadVentas (a3^.hi3, maxCod, maxVentas);
+		vent:= 0;
+		cuentoVentas (a3^.dato3.listaa, vent);
+		calculoMax (vent, a3^.dato3.codigo, maxCod, maxVentas);
+		mayorCantidadVentas (a3^.hd3, maxCod, maxVentas);
+	end;
+end;
+//------------------ busca codigo maximo del que tiene mayor cantidad de ventas del arbol 3 ---------------------
+
+// programa principal
 var
 	a1: arbol1;
 	a2: arbol2;
+	a3: arbol3;
+	fechaa: fecha;
+	cantProductos: integer;
+	codMax, maxVentas: integer;
 begin
 	a1:= nil;
 	a2:= nil;
+	a3:= nil;
+	cantProductos:= 0;
+	maxVentas:= -1;
+	codMax:= -1;
 	Randomize;
-	generarArboles (a1,a2); //inciso A
+	generarArboles (a1,a2,a3);
+	imprimirArboles(a1,a2,a3);
+	write ('Ingrese una dia: ');
+	read (fechaa.dia);
+	write ('Ingrese un mes: ');
+	read (fechaa.mes);
+	write ('Ingrese una anio: ');
+	read (fechaa.anio);
+	buscardeEsaFecha (a1, fechaa, cantProductos);
+	writeln;
+	write ('La cantidad total de productos vendidos en esa fecha es: ' , cantProductos);
+	writeln;
+	write ('El codigo de producto con mayor cantidad total de unidades vendidas es: ' , buscarcodMax (a2));
+	mayorCantidadVentas (a3, codMax, maxVentas);
+	writeln;
+	write ('El codigo de producto con mayor cantidad de ventas es: ' , codMax);
 end.
