fc417c509ccb03889b20aa18848ec62aaf942ae9
branrodriguezz <bran3rodriguez@gmail.com>
2024-09-19 09:22:46 -0300

    Ejercicio 4.java

--- Objetos/Practica 1/Ejercicio 4.java
+++ Objetos/Practica 1/Ejercicio 4.java
@@ -2,7 +2,7 @@ package tema1;
 
 import PaqueteLectura.GeneradorAleatorio;
 
-public class Ej04  {
+public class Ej4  {
     /*
     4- Un edificio de oficinas está conformado por 8 pisos (1..8) y 4 oficinas por piso (1..4). 
     Realice un programa que permita informar la cantidad de personas que concurrieron a cada oficina
@@ -14,53 +14,57 @@ public class Ej04  {
 
 public static void main(String[] args) {
     
-    int [][] matriz = new int [8+1][4+1];
+    int [][] matriz = new int [8][4];
     
         int i,j;
         
         //prueba de la matriz 
         
-        for (i = 1; i <= 8; i++) {
-            for (j = 1; j <= 4; j++)
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 4; j++)
                 matriz [i][j] = GeneradorAleatorio.generarInt(3);
         }
         
         System.out.print ("---- MATRIZ DE ENTEROS (8X4) ----");
         
-        for (i = 1; i <= 8; i++) {
+        for (i = 0; i < 8; i++) {
             System.out.println ();
-            for (j = 1; j <= 4; j++)
+            for (j = 0; j < 4; j++)
                 System.out.print ("Pos: (" + i + "," + j + ") " + matriz [i][j] + " | ");
                 }
         System.out.println ();
         
         //inicializo la matriz de enteros 
         
-        for (i = 1; i <= 8; i++) {
-            for (j = 1; j <= 4; j++)
+        for (i = 0; i < 8; i++) {
+            for (j = 0; j < 4; j++)
                 matriz [i][j] = 0;
         }
-        
+    
+        GeneradorAleatorio.iniciar();
+    
         //cargo la matriz
         int piso;
         int oficina;
-        piso = GeneradorAleatorio.generarInt(9+1);
-        oficina = GeneradorAleatorio.generarInt(4+1);
-        
+        piso = GeneradorAleatorio.generarInt(9) + 1;
+        oficina = GeneradorAleatorio.generarInt(4) + 1;
         while (piso != 9) {
-            matriz [piso][oficina]= matriz [piso][oficina] + 1;
-            piso = GeneradorAleatorio.generarInt(9+1);
-            oficina = GeneradorAleatorio.generarInt(4+1);
+            matriz [piso-1][oficina-1]= matriz [piso - 1][oficina - 1] + 1;
+             System.out.println ("Piso: " + (piso) );
+             System.out.println ("Oficina: " + (oficina) );
+        
+            piso = GeneradorAleatorio.generarInt(9) + 1;
+            oficina = GeneradorAleatorio.generarInt(4) + 1;
         }
         
         //imprimo matriz de personas
         System.out.println();
         System.out.print ("---- MATRIZ DE PERSONAS (8X4) ----");
-        for (i = 1; i <= 8; i++) {
+        for (i = 0; i < 8; i++) {
             System.out.println();
-            System.out.println ("Piso: " + i);
-            for (j = 1; j <= 4; j++) {
-                System.out.print ("Oficina: " + j + " | ");
+            System.out.println ("Piso: " + (i+1));
+            for (j = 0; j < 4; j++) {
+                System.out.print ("Oficina: " + (j+1) + " | ");
                 System.out.print ("Personas: " + matriz [i][j] + " | ");
             }
             System.out.println ();
