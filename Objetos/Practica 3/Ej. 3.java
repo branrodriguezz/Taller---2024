b90b33ff429511adbf529b08c135ed6abf1a5f5e
branrodriguezz <bran3rodriguez@gmail.com>
2024-10-03 14:40:23 -0300

    Update Ej. 3.java

--- Objetos/Practica 3/Ej. 3.java
+++ Objetos/Practica 3/Ej. 3.java
@@ -4,6 +4,159 @@
  */
 package tema3;
 
+/**
+ *
+ * @author branroodriguez
+ */
+public class Autor {
+    private String nombre;
+    private String biografia;
+    private String origen;
+    
+    public Autor (String unNombre, String unaBiografia, String unOrigen){
+        nombre = unNombre;
+        biografia = unaBiografia;
+        origen = unOrigen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getBiografia() {
+        return biografia;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setNombre(String unnombre) {
+        nombre = unnombre;
+    }
+
+    public void setBiografia(String unabiografia) {
+        biografia = unabiografia;
+    }
+
+    public void setOrigen(String unorigen) {
+        origen = unorigen;
+    }
+    
+    @Override
+    public String toString (){
+        String aux = "- Nombre " + nombre + " - Biografia " + biografia + " - Origen " + origen;
+        return aux;
+    }
+    
+    
+}
+/*
+Clase Libro a la cual se agregaron constructores. 
+ */
+package tema3;
+
+/**
+ *
+ * @author vsanz
+ */
+public class Libro {
+    /*
+    2-A- Modifique la clase Libro.java (carpeta tema3) para ahora considerar que el primer autor 
+    es un objeto instancia de la clase Autor.
+    Implemente la clase Autor, sabiendo que se caracterizan por nombre, biografía y origen y 
+    que deben permitir devolver/modificar el valor de sus atributos y devolver una representación 
+    String formada por nombre, biografía y origen.
+    Luego realice las modificaciones necesarias en la clase Libro.
+    */
+   private String titulo;
+   private Autor primerAutor; 
+   private String editorial;
+   private int añoEdicion;
+   private String ISBN; 
+   private double precio; 
+     
+    
+    public Libro(  String unTitulo,  Autor unPrimerAutor, String unaEditorial, 
+    int unAñoEdicion, String unISBN, double unPrecio){
+         titulo = unTitulo;
+         primerAutor = unPrimerAutor;
+         editorial = unaEditorial; 
+         añoEdicion= unAñoEdicion;
+         ISBN =  unISBN;
+         precio = unPrecio;
+    }
+    
+    public Libro(  String unTitulo,  String unaEditorial, Autor unPrimerAutor, String unISBN){
+         titulo = unTitulo;
+         editorial = unaEditorial; 
+         añoEdicion= 2015;
+         primerAutor = unPrimerAutor;
+         ISBN =  unISBN;
+         precio = 100;
+    }
+    
+    public Libro(){
+   
+    }
+        
+    public String getTitulo(){
+        return titulo;
+    }
+  
+    public String getEditorial(){
+        return editorial;
+    }
+    public int getAñoEdicion(){
+        return añoEdicion;
+    }
+  
+    public Autor getPrimerAutor(){
+        return primerAutor;
+    } 
+    public String getISBN(){
+        return ISBN;
+    } 
+    public double getPrecio(){
+        return precio;
+    }
+   
+    public void setTitulo(String unTitulo){
+        titulo = unTitulo;
+    }
+   
+    public void setEditorial(String unaEditorial){
+         editorial = unaEditorial;
+    }
+    public void setAñoEdicion(int unAño){
+         añoEdicion = unAño;
+    }
+   
+    public void setPrimerAutor(Autor unPrimerAutor){
+         primerAutor=unPrimerAutor;
+    } 
+    public void setISBN(String unISBN){
+         ISBN=unISBN;
+    } 
+    public void setPrecio(double unPrecio){
+         precio=unPrecio;
+    }
+   
+    
+   @Override
+    public String toString(){
+        String aux;
+        aux= " Titulo: " + titulo + " | Autor " + primerAutor + " | Editorial: " + editorial + " | Año de edicion: " + añoEdicion + " | ISBN: " + ISBN + " | Precio: " + precio + " | ";
+       return ( aux);
+    }
+    
+    public String devolverAutor(){
+        String a;
+        a = "| Autor : " + primerAutor;
+        return a;
+    }
+        
+}
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -22,12 +175,11 @@ public class Estante {
     //constructor que permita iniciar el estante sin libros.
     public Estante () {
         vector = new Libro [20];
-        int dimL = 0;
+        dimL = 0;
     }
     
     //metodos 
     
-
     public Libro[] getVector() { //DEVUELVE UN LIBRO
         return vector;
     }
@@ -36,13 +188,12 @@ public class Estante {
     public int getDimL() { //DEVUELVE LA CANTIDAD DE LIBROS
         return dimL;
     }
-
+    
+    private void setdimL (int undimL){
+        dimL = undimL;
+    }
     public void setVector(Libro[] unVector) { //MODIFICO UN LIBRO
         vector = unVector;
-    }
-
-    public void setDimL(int unDimL) { //MODIFICO LA CANTIDAD DE LIBROS 
-        dimL = unDimL;
     }
     
     //(ii) devolver si el estante está lleno
@@ -61,28 +212,36 @@ public class Estante {
     }
     
     //(iv) devolver el libro con un título particular que se recibe.
-    public Libro tituloLibro (String titulo) {
-        int i;
-        for (i = 0; i < dimL; i++) { //preguntar por el metodo equals, puedo usarlo en este caso?
-            if (vector[i].getTitulo().equals(titulo)) {
-                return vector [i]; //preguntar con while
+    public String tituloLibro (String titulo) {
+        int i = 0;
+        Boolean encontre = false;
+        String aux = "";
+        while (i < dimL && encontre.equals(false)) { 
+                if (vector[i].getTitulo().equals(titulo)) {
+                    encontre = true;
+                    aux = vector[i].devolverAutor();
+                }   
+                i++;
             }
+        if (encontre.equals(true))
+            return aux;
+        else {
+            System.out.print("No existe ese libro");
+            return null;
         }
-        return null;
     }
+        
     
+    @Override
     public String toString() {
         String info = "";
         for (int i = 0; i < dimL; i++) {
             info = info  + " | Libro " + (i+1) + " | " + vector[i].toString() + "\n";
             info = info + " ------------------------------------------------------------------------------------------------- ";
         }
-        
         return info;
     }
 }
-
-//programa
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -147,10 +306,11 @@ public class ej03 {
         
         System.out.println (" ----------------- Estante de libros ------------------- ");
         System.out.println ();
-        estanTe.imprimir(); //imprimo el estante de libros
+        System.out.print (estanTe.toString()); //imprimo el estante de libros
         
         //busco e informo el autor del libro mujercitas
         System.out.println ();
-        System.out.println ("Libro Mujercitas: " + estanTe.tituloLibro("Mujercitas").getPrimerAutor());
+        System.out.println ("Libro Mujercitas: " + estanTe.tituloLibro("Mujercitas"));
+        
     }
 }
